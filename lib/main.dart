16d8a05cbd47be1dc8d7745901299c228ffe35df
Nzabinesha <m.nzabinesh@alustudent.com>
2026-06-11 14:07:07 +0200

    added multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'core/theme/app_theme.dart';
+import 'providers/event_provider.dart';
+import 'providers/profile_provider.dart';
 import 'screens/onboarding/landing_screen.dart';
 
 void main() {
@@ -11,11 +14,17 @@ class AluConnectApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ALU Connect+',
-      theme: AppTheme.darkTheme,
-      home: const LandingScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => EventProvider()),
+        ChangeNotifierProvider(create: (_) => ProfileProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'ALU Connect+',
+        theme: AppTheme.darkTheme,
+        home: const LandingScreen(),
+      ),
     );
   }
 }
